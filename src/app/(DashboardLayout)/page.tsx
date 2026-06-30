ba7911cbd6196760f48bd5caa17029f821c5b35e
GreenCode668 <derek96langston1218@outlook.com>
2026-06-30 03:03:58 +0200

    chore: drop unused modernize demo cards from dashboard (charts, transactions, products, footer)

--- src/app/(DashboardLayout)/page.tsx
+++ src/app/(DashboardLayout)/page.tsx
@@ -1,12 +1,4 @@
-
-import React from "react";
 import TasksAttention from "../components/dashboard/TasksAttention";
-import { YearlyBreakup } from "../components/dashboard/YearlyBreakup";
-import { MonthlyEarning } from "../components/dashboard/MonthlyEarning";
-import { RecentTransaction } from "../components/dashboard/RecentTransaction";
-import { ProductPerformance } from "../components/dashboard/ProductPerformance";
-import { Footer } from "../components/dashboard/Footer";
-import { BestSeller } from "../components/dashboard/BestSeller";
 import TopCards from "../components/dashboard/TopCards";
 import { WelcomeBanner } from "../components/dashboard/WelcomeBanner";
 import QuickAccess from "../components/dashboard/QuickAccess";
@@ -15,50 +7,26 @@ import AgendaDay from "../components/dashboard/AgendaDay";
 
 const page = () => {
   return (
-    <>
-      <div className="grid grid-cols-12 gap-6">
-        <div className="col-span-12">
-          <TopCards />
-        </div>
-        <div className="col-span-12">
-          <WelcomeBanner />
-        </div>
-        <div className="col-span-12">
-          <QuickAccess />
-        </div>
-        <div className="col-span-12">
-          <TreatmentSummary />
-        </div>
-        <div className="col-span-12">
-          <AgendaDay />
-        </div>
-        <div className="lg:col-span-8 col-span-12">
-          <TasksAttention />
-        </div>
-        <div className="lg:col-span-4 col-span-12">
-          <div className="grid grid-cols-12 gap-6">
-            <div className="col-span-12">
-              <YearlyBreakup />
-            </div>
-            <div className="col-span-12">
-              <MonthlyEarning />
-            </div>
-          </div>
-        </div>
-        <div className="lg:col-span-4 col-span-12">
-          <RecentTransaction />
-        </div>
-        <div className="lg:col-span-8 col-span-12 flex">
-          <ProductPerformance />
-        </div>
-        <div className="col-span-12">
-          <BestSeller />
-        </div>
-        <div className="col-span-12">
-          <Footer />
-        </div>
+    <div className="grid grid-cols-12 gap-6">
+      <div className="col-span-12">
+        <TopCards />
       </div>
-    </>
+      <div className="col-span-12">
+        <WelcomeBanner />
+      </div>
+      <div className="col-span-12">
+        <QuickAccess />
+      </div>
+      <div className="col-span-12">
+        <TreatmentSummary />
+      </div>
+      <div className="col-span-12">
+        <AgendaDay />
+      </div>
+      <div className="col-span-12">
+        <TasksAttention />
+      </div>
+    </div>
   );
 };
 
--- src/app/components/dashboard/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link"
-
-export const Footer = () => {
-    return (
-        <>
-            <p className="text-base text-center text-bodytext font-medium">Design and Developed by <Link href="https://adminmart.com/" target="_blank" className="text-primary font-normal underline hover:text-primaryemphasis">adminmart.com</Link> </p>
-        </>
-    )
-}
